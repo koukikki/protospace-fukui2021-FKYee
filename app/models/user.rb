450c593e2069d91f9ce9d4a2ab958b13dec3d132
AtsushiKondou <89501142+AtsushiKondou@users.noreply.github.com>
2021-09-11 11:56:12 +0900

    has_manyの単数を複数に修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :prototype
-         has_many :comment
+         has_many :prototypes
+         has_many :comments
 end
