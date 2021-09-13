585ad16b5c2d93b0a5c4823019e9e300a04c7e3c
AtsushiKondou <89501142+AtsushiKondou@users.noreply.github.com>
2021-09-13 11:53:44 +0900

    単体テストの作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,12 @@ RSpec.describe User, type: :model do
         expect(@user).to be_valid
       end
 
+      it 'passwordが6文字以上であれば登録できる' do
+        @user.password = "000000"
+        @user.password_confirmation = "000000"
+        expect(@user).to be_valid
+      end
+
     end
 
     context '新規登録できない場合' do
@@ -50,6 +56,33 @@ RSpec.describe User, type: :model do
         @user.post = " "
         @user.valid?
         expect(@user.errors.full_messages).to include("Post can't be blank")
+      end
+
+      it 'passwordが5文字以下であれば登録できない' do
+        @user.password = "00000"
+        @user.password_confirmation = "00000"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
+
+      it 'passwordとpassword_confirmationが不一致では登録できない' do
+        @user.password = "000000"
+        @user.password_confirmation = "111111"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+
+      it 'emailが重複していると登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user, email: @user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
+      end
+
+      it 'emailに@が含まれていないと登録できない' do
+        @user.email = "test.com"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
       end
 
     end
