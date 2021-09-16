f72ceb7931cf09dae099288d05c563bde1070f5d
AtsushiKondou <89501142+AtsushiKondou@users.noreply.github.com>
2021-09-16 17:14:50 +0900

    コメント投稿機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   
   root to: 'prototypes#index'
 
-  resources :prototypes, only: [:new, :create, :show]
-
+  resources :prototypes, only: [:new, :create, :show] do
+    resources :comments, only: :create
+  end
 end
