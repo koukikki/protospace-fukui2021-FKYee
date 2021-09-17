d52df3dbd994017b5034dbf290122b5c5cb152f3
AtsushiKondou <89501142+AtsushiKondou@users.noreply.github.com>
2021-09-17 17:38:36 +0900

    ユーザ詳細ページの作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,6 @@
+class UsersController < ApplicationController
+  def show
+    @user = current_user
+    @prototypes = current_user.prototypes
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root to: 'prototypes#index'
 
   resources :prototypes, only: [:new, :create, :show]
-
+  resources :users, only: :show
 end
