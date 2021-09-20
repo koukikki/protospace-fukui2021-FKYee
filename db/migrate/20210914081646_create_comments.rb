de3cb4a34e1969738dff87dbc01991b1760e2040
AtsushiKondou <89501142+AtsushiKondou@users.noreply.github.com>
2021-09-20 15:19:46 +0900

    commentsテーブルの修正

--- db/migrate/20210914081646_create_comments.rb
+++ db/migrate/20210914081646_create_comments.rb
@@ -3,7 +3,8 @@ class CreateComments < ActiveRecord::Migration[6.0]
     create_table :comments do |t|
       t.references :user,           null: false,foreign_key: true
       t.references :prototype,      null: false,foreign_key: true
-      t.text   :text,               null: false
+      t.text   :text,               
+      t.integer :score,             null: false
       t.timestamps
     end
   end
