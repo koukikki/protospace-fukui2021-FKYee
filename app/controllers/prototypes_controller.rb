676b1d4535ee846c98cf1a8c0a948bcfead4a2e1
k.f.koki <86524836+koukikki@users.noreply.github.com>
2021-09-20 15:21:32 +0900

    投稿したユーザーにのみ削除できる機能

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
 
   before_action :set_prototype, only: [:show, :edit, :update]
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
 
   def index
     @prototypes = Prototype.all
