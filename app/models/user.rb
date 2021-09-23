c18aa3275a44227d276805dc412ee715397c03fd
k.f.koki <86524836+koukikki@users.noreply.github.com>
2021-09-23 14:21:36 +0900

    Rubocopを実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
   has_many :prototypes
   has_many :comments
@@ -20,9 +20,9 @@ class User < ApplicationRecord
 
     user = User.where(email: auth.info.email).first_or_initialize(
       name: auth.info.name,
-        email: auth.info.email
+      email: auth.info.email
     )
-    
+
     if user.persisted?
       sns.user = user
       sns.save
